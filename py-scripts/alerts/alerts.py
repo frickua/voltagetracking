7321f9cd92849750e310355b9a87e67f3cc6b5e5
frickua <frick.ua@gmail.com>
2026-04-06 13:34:15 +0300

    Alert workflow draft

---
+++ py-scripts/alerts/alerts.py
@@ -0,0 +1,36 @@
+import json
+import os
+import sys
+
+import requests
+from datetime import datetime, date, timezone
+from dateutil import parser, tz
+from supabase import Client, create_client
+
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
+EVENT_PATH = os.getenv('GITHUB_EVENT_PATH')
+TZ: "Europe/Kyiv"
+
+
+###############################################################
+###############################################################
+###############################################################
+
+with open(EVENT_PATH) as f:
+    payload = json.load(f)
+
+print(payload)
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+response = supabase.table("alerts_fingerprints").select('fingerprint').execute()
+
+fingerprints = {row["fingerprint"] for row in response.data}
+
+
+
+
+
+
