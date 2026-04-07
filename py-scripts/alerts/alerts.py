e4e1cfccc7dacdc04b41d56cd955cdf7666f93ae
frickua <frick.ua@gmail.com>
2026-04-07 11:12:09 +0300

    Alerts mvp

--- py-scripts/alerts/alerts.py
+++ py-scripts/alerts/alerts.py
@@ -1,10 +1,8 @@
 import json
 import os
-import sys
 
 import requests
-from datetime import datetime, date, timezone
-from dateutil import parser, tz
+from datetime import datetime, timezone, timedelta
 from supabase import Client, create_client
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
@@ -14,26 +12,69 @@ EVENT_PATH = os.getenv('GITHUB_EVENT_PATH')
 TZ: "Europe/Kyiv"
 
 
-###############################################################
-###############################################################
-###############################################################
+def update_fingerprints(fingerprints):
+    # Update fingerprints
+    old_fingerprints_ts = datetime.now(timezone.utc) - timedelta(hours=5)
+
+    # Delete rows older than 5 hours
+    supabase.table("alerts-fingerprints").delete().lt("created_at", old_fingerprints_ts.isoformat()).execute()
+    if len(fingerprints) > 0:
+        data = [{"fingerprint": v} for v in fingerprints]
+        supabase.table("alerts-fingerprints").upsert(data, on_conflict="fingerprint").execute()
+
+def send_tg_msg(chat_id, topic=None, txt=None, parse_mode='TEXT'):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": txt,
+        "message_thread_id": topic,
+        "parse_mode": parse_mode
+    }
+    response = requests.post(url, data=payload)
+    print(response.json())
 
 with open(EVENT_PATH) as f:
-    print('########################')
-    print(f.read())
-    print('########################')
     payload = json.load(f)
 
-print(payload['client_payload'])
+alerts = payload['client_payload']['alerts']
+print(payload['client_payload']['alerts'])
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+response = supabase.table("tg_channels").select('auth_key', 'chat_id', 'tg_topic').execute()
+keys_to_channels = {row["auth_key"]: {"chat_id": row["chat_id"], "topic": row['tg_topic']} for row in response.data}
 
 response = supabase.table("alerts-fingerprints").select('fingerprint').execute()
-
 fingerprints = {row["fingerprint"] for row in response.data}
 
+for alert in alerts:
+    status = alert['status']
+    value = alert['values']['A']
+    fingerprint = alert['fingerprint']
+    key = alert['labels']['key']
+    phase = alert['labels']['phase']
+    channel = keys_to_channels.get(key)
+    print(f"{status} {value} {fingerprint} {key}")
+    info_link = "<i>\n<blockquote expandable>📌Попередження - інформативне, ваша оселя може бути підключена до іншої фази або навіть до іншої підстанціі.\nСприймайте це сповіщення як погоду - загальну картину стану енергоситеми зараз.\nДля надійного захисту використовуйте <b>реле напруги</b> та <b>стабілізатори</b><a href=\"https://voltagetracking.frick.net.ua/voltage-infographic.html?v=1\">.</a></blockquote></i>"
+    if status == 'firing':
+        if fingerprint not in fingerprints: # Verify that alert already processed
+            fingerprints.add(fingerprint)
+            if channel:
+                txt = ""
+                if value > 230:
+                    txt = "🔺 Підвищена "
+                else:
+                    txt = "🔻 Знижена "
+                send_tg_msg(channel['chat_id'], channel['topic'], f"{txt} напруга: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
+            else:
+                print(f"Channel not found {key} {fingerprint}")
+    elif status == 'resolved':
+        if fingerprint not in fingerprints:
+            fingerprints.add(fingerprint)
+            send_tg_msg(channel['chat_id'], channel['topic'], f"✅ Напруга стабілізувалась: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
+    else:
+        print(f"Unknown status: {status}")
 
-
+update_fingerprints(fingerprints)
 
 
 
