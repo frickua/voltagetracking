cc28d8233ca87614547436a11a607b3a656f820b
frickua <frick.ua@gmail.com>
2026-04-07 11:25:34 +0300

    fix channel registration

--- py-scripts/channel-registration/channel-registration.py
+++ py-scripts/channel-registration/channel-registration.py
@@ -49,23 +49,30 @@ def send_tg_msg(chat_id, txt, parse_mode='TEXT'):
 
 
 
-def send_welcome_msg(rows):
-    send_tg_msg(rows[0]['tg_user_id'],
-f"""👋 Привіт! Дякую, за цікавість до проєкту моніторингу напруги в енергосистемі.\n 
+def send_welcome_msg(user_id):
+    response = (supabase.table("tg_channels")
+                .select('auth_key', 'tg_user_id', 'chat_name')
+                .eq("tg_user_id", user_id).execute())
+    if response and len(response.data) > 0:
+        data = response.data[0]
+        if  data['auth_key'] and data['tg_user_id']:
+            send_tg_msg(response.data[0]['tg_user_id'],
+        f"""👋 Привіт! Дякую, за цікавість до проєкту моніторингу напруги в енергосистемі.\n 
             Цей проєкт дозволить тобі та твоїм сусідам, слідкувати за рівнем напруги у вашій оселі, та попередити аварійні ситуації.\n\n
             Ти можеш додати бота в кілька своїх груп, і він автоматично надсилатиме одні і ті ж дані в них\n
             Твої унікальні ключі для надсилання даних:\n
-            {"\n".join(f"- {row['chat_name']}: {row['auth_key']}" for row in rows)}
+            {"\n".join(f"- {row['chat_name']}: {row['auth_key']}" for row in response.data)}
             Тепер ти можеш надсилати дані своєї напруги в ситему\n\n
             Для цього використовуй HTTP POST запит https://metrics_voltagetracking.frick.net.ua/api/v1/push/influx/write\n
             Обов'язково додай заголовок, для авторизаціі
             <pre>Authorization: Basic {{TOKEN}}</pre>
             Тіло запиту(body):
-            <pre><code>voltage,key={rows[0]['auth_key']},phase=1 value=242.7</code></pre>\n\n
+            <pre><code>voltage,key={response.data[0]['auth_key']},phase=1 value=242.7</code></pre>\n\n
             Для того аби вказати боту в який топік(вкладку) групи писати, просто надійшли в цей топік ім'я бота(тегни його) <pre>@voltage_tracking_bot</pre> Бот сам видалить це повідомлення - коли налаштується\n\n
             ❗️Звернись за секретним токеном {{TOKEN}} до @frickua, наразі це відбувається вручну\n\n
             ❔Більше інформаціі тут: https://voltagetracking.frick.net.ua""", 'HTML')
-
+    else:
+        return False
 
 offset = None
 
@@ -95,13 +102,11 @@ for update in resp["result"]:
                      "tg_user_info": f"@{chat_member_from['username']} ({chat_member_from['first_name']})",
                      "auth_key": auth_key}).execute()
             except APIError as e:
-                if e.message and 'duplicate key' in e.message:
-                    response = supabase.table("tg_channels").select('auth_key').eq("chat_id", chat['id']).execute()
-                auth_key = response.data[0]['auth_key']
+               print(f"ERROR. Can not add channel: {e} ")
 
             print(response)
 
-            send_welcome_msg(chat_member_from['id'], auth_key)
+            send_welcome_msg(chat_member_from['id'])
 
         elif new_status in ["left", "kicked"]:
             print(f"❌ Bot removed from channel: {chat['title']} ({chat['id']})")
@@ -118,14 +123,7 @@ for update in resp["result"]:
         if 'text' in msg:
             msg_txt = msg['text']
             if msg_txt == '/start':
-                response = (supabase.table("tg_channels")
-                            .select('auth_key', 'tg_user_id', 'chat_name')
-                            .eq("tg_user_id", msg['from']['id']).execute())
-                if response and len(response.data) > 0:
-                    data = response.data[0]
-                    if  data['auth_key'] and data['tg_user_id']:
-                        send_welcome_msg(response.data)
-                else:
+                if not send_welcome_msg(msg['from']['id']):
                     send_tg_msg(msg['from']['id'],
                                 "😔 Вибач, я не знайшов твій канал або групу. Додай мене адміністратором до каналу або групи в якій ти хочеш бачити графіки та сповіщення про напругу.\n"
                                 "Наразі лише той адміністратор який додав бота \"прив'язується\" до системи.\n"
