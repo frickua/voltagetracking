32ca7f74a1225ee9ad53e2f28844cbb4a65871f0
frickua <frick.ua@gmail.com>
2026-04-07 15:46:12 +0300

    Alerts should send for multiple channels with same key

--- py-scripts/alerts/alerts.py
+++ py-scripts/alerts/alerts.py
@@ -22,6 +22,10 @@ def update_fingerprints(fingerprints):
         data = [{"fingerprint": k, "status": v} for k, v in fingerprints.items()]
         supabase.table("alerts-fingerprints").upsert(data, on_conflict="fingerprint,status").execute()
 
+def send_tg_msgs(chats, txt, parse_mode='TEXT'):
+    for chat in chats:
+        send_tg_msg(chat['chat_id'], chat['tg_topic'], txt, parse_mode)
+
 def send_tg_msg(chat_id, topic=None, txt=None, parse_mode='TEXT'):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
@@ -41,7 +45,12 @@ print(payload['client_payload']['alerts'])
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 response = supabase.table("tg_channels").select('auth_key', 'chat_id', 'tg_topic').execute()
-keys_to_channels = {row["auth_key"]: {"chat_id": row["chat_id"], "topic": row['tg_topic']} for row in response.data}
+keys_to_channels = {}
+for row in response.data:
+    keys_to_channels.setdefault(row["auth_key"], []).append({
+        "chat_id": row["chat_id"],
+        "topic": row["tg_topic"]
+    })
 
 response = supabase.table("alerts-fingerprints").select('fingerprint', 'status').execute()
 fingerprints = {row["fingerprint"]: row["status"] for row in response.data}
@@ -52,25 +61,25 @@ for alert in alerts:
     fingerprint = alert['fingerprint']
     key = alert['labels']['key']
     phase = alert['labels']['phase']
-    channel = keys_to_channels.get(key)
+    channels = keys_to_channels.get(key)
     print(f"{status} {value} {fingerprint} {key}")
     info_link = "<i>\n<blockquote expandable>📌Попередження - інформативне, ваша оселя може бути підключена до іншої фази або навіть до іншої підстанціі.\nСприймайте це сповіщення як погоду - загальну картину стану енергоситеми зараз.\nДля надійного захисту використовуйте <b>реле напруги</b> та <b>стабілізатори</b><a href=\"https://voltagetracking.frick.net.ua/voltage-infographic.html?v=1\">.</a></blockquote></i>"
     if status == 'firing':
         if fingerprint not in fingerprints: # Verify that alert already processed
             fingerprints[fingerprint] = status # Add new fingerprint
-            if channel:
+            if channels and len(channels) > 0:
                 txt = ""
                 if value > 230:
                     txt = "🔺 Підвищена "
                 else:
                     txt = "🔻 Знижена "
-                send_tg_msg(channel['chat_id'], channel['topic'], f"{txt} напруга: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
+                send_tg_msg(channels, f"{txt} напруга: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
             else:
                 print(f"Channel not found {key} {fingerprint}")
     elif status == 'resolved':
         if fingerprint not in fingerprints or fingerprints[fingerprint] != 'resolved':
             fingerprints[fingerprint] = status
-            send_tg_msg(channel['chat_id'], channel['topic'], f"✅ Напруга стабілізувалась: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
+            send_tg_msg(channels, f"✅ Напруга стабілізувалась: {value} Вольт\nФаза: {phase}\n\n{info_link}", 'HTML')
     else:
         print(f"Unknown status: {status}")
 
